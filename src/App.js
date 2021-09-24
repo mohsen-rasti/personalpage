a1f23bb9ec7e7c2e5e355d79e7df1f8a4f6b0852
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-09-24 23:21:18 +0330

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+
+import login from "./component/login/login";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/login' component ={login} />
+        
+
+      </Switch>
+      
+  
+    </Router>
   );
 }
-
-export default App;
