ed06c72b069d42848e0d708ddbe3ae687de3e62b
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-09-27 22:52:47 +0330

    menu

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,23 +5,40 @@ import "../App.css";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+<div className="container">
         <a className="navbar-brand" href="#"><img className="logo" src={pic} alt="pic..."/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Home </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Services Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">How work</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfilio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
             </li>
             
        
           </ul>
           
+        </div>
         </div>
       </nav>
     
