3d8872c03703a67dc31d3a853a6e6f3470d8af7e
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-09-28 14:39:58 +0330

    navbar add finish

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import pic from "../pic.jpg";
 import "../App.css";
+///font aswomw//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars} from "@fortawesome/free-solid-svg-icons";
+
 
 function Navbar() {
     return (
@@ -8,7 +12,7 @@ function Navbar() {
 <div className="container">
         <a className="navbar-brand" href="#"><img className="logo" src={pic} alt="pic..."/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+<FontAwesomeIcon icon={faBars} style={{color:"#fff"}} />
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
